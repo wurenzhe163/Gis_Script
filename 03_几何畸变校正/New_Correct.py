edda8768a32a9655a304b241054a9d38465818ae
caohualin <chl2176518050@gmail.com>
2023-08-31 10:31:36 +0800

    被动几何畸变识别（方法2、识别函数删掉了cal_image_scale参数且将AOI参数改成AOIbuff、AOIbuff使用2000像素测试）

--- 03_几何畸变校正/New_Correct.py
+++ 03_几何畸变校正/New_Correct.py
@@ -200,7 +200,7 @@ def Distortion(alpha_rRad, theta_iRad, image,proj, height, AOI_buffer):
 
 
 # 线性几何畸变校正
-def Line_Correct(cal_image,AOI,Templist, orbitProperties_pass, proj, scale:int,cal_image_scale:int):
+def Line_Correct2(cal_image,AOI,Templist, orbitProperties_pass, proj, scale:int,cal_image_scale:int):
 
     line_points_list = []
     LPassive_layover_linList = []
@@ -332,3 +332,161 @@ def Line_Correct(cal_image,AOI,Templist, orbitProperties_pass, proj, scale:int,c
 
     return LeftLayover.toInt8(), RightLayover.toInt8(), Shadow.toInt8()
 
+# 被动几何畸变识别（方法2）
+def Line_Correct_test(cal_image,AOI_buffer,Templist, orbitProperties_pass, proj, scale:int):
+    line_points_list = []
+    # LPassive_layover_linList = []
+    # RPassive_layover_linList = []
+    # Shadow_linList = []
+
+    for eachLine in tqdm(Templist):
+        # 求与辅助线相交的像素值,求取极值点位置
+        LineImg = cal_image.select(
+            ['height', 'layover', 'shadow', 'incAngle', 'x', 'y', 'longitude', 'latitude']).clip(eachLine)
+        LineImg_point = LineImg.reduceRegion(
+            reducer=ee.Reducer.toList(),
+            geometry=cal_image.geometry(),
+            scale=30,
+            maxPixels=1e13)
+        line_points_list.append(LineImg_point)
+    list_of_dicts = ee.List(line_points_list).getInfo()  # 需转换数据到本地，严重耗时
+    # 分别为右侧被动叠掩点、左侧被动叠掩点和被动阴影点经纬度列表
+    r_lon_sum = []
+    r_lat_sum = []
+    l_lon_sum = []
+    l_lat_sum = []
+    sh_lon_sum = []
+    sh_lat_sum = []
+    # 逐视线检索被动畸变
+    for PointDict in tqdm(list_of_dicts):
+        if orbitProperties_pass == 'ASCENDING':
+            order = np.argsort(PointDict['x'])
+        elif orbitProperties_pass == 'DESCENDING':
+            order = np.argsort(PointDict['x'])[::-1]
+        PointDict = {k: np.array(v)[order] for k, v in PointDict.items()}  # 将视线上的像元按照x，从小到大排序
+        PointDict['x'], PointDict['y'] = PointDict['x'] * \
+                                         10, PointDict['y'] * 10  # 像素行列10m分辨率，由proj得
+        EasyIndex = lambda Data, Index, *Keys: [Data[key][Index] for key in Keys]
+        # 被动阴影像元识别
+        if len(np.where(PointDict['shadow'] == 1)[0]) != 0:
+            # 利用grad_fn函数识别出主动阴影起始索引和结束索引
+            shadow_grad_fn = lambda shadow_array: [shadow_array[i + 1] - shadow_array[i] for i in
+                                                   range(len(shadow_array) - 1)]
+            PointDict_shadow_padding = np.insert(PointDict['shadow'], 0, 0)
+            shadow_grad = np.array(shadow_grad_fn(PointDict_shadow_padding))
+            # 防止PointDict['shadow']最后是以1结尾，导致-1比1个数少一个，且1结尾没有检测被动阴影必要，所以直接去掉
+            if PointDict_shadow_padding[-1] == 1:
+                shadow_start_index = np.where(shadow_grad == 1)[0][:-1]
+            else:
+                shadow_start_index = np.where(shadow_grad == 1)[0]
+            shadow_range_index = np.where(shadow_grad == -1)[0]
+            # 以主动阴影的起始像元位置为参考点
+            sh_h, sh_x, s_y, sh_lon, sh_lat, sh_angle = EasyIndex(
+                PointDict, shadow_start_index, 'height', 'x', 'y', 'longitude', 'latitude', 'incAngle')
+            # 在某一视线中，逐段主动阴影进行分析
+            if len(shadow_start_index) != 0:
+                for i in range(shadow_start_index.size):
+                    start_sh_index = shadow_start_index[i]
+                    range_sh_index = shadow_range_index[i]
+                    index_Shadow = []
+                    # 确定检索像元范围
+                    if range_sh_index + 50 < len(PointDict['shadow']):
+                        rangeIndex_shadow = range(range_sh_index, range_sh_index + 50)
+                    else:
+                        rangeIndex_shadow = range(range_sh_index, len(PointDict['shadow']))
+                    if len(rangeIndex_shadow) != 0:
+                        sh_Range_h, sh_Range_x, sh_Range_y, sh_Range_lon, sh_Range_lat = EasyIndex(
+                            PointDict, rangeIndex_shadow, 'height', 'x', 'y', 'longitude', 'latitude')
+                        shadow_angle_iRad = np.arctan((sh_h[i] - sh_Range_h) / np.sqrt(
+                            np.square(sh_Range_x - sh_x[i]) + np.square(sh_Range_y - s_y[i])))
+                        shadow_angle = shadow_angle_iRad * 180 / math.pi
+                        index_Shadow = np.where(shadow_angle > (90 - sh_angle[i]))[0]
+
+                    if len(index_Shadow) != 0:
+                        tlon_shadow = sh_Range_lon[index_Shadow]
+                        tlat_shadow = sh_Range_lat[index_Shadow]
+                        for j in range(len(tlon_shadow)):
+                            sh_lon_sum.append(tlon_shadow[j])
+                            sh_lat_sum.append(tlat_shadow[j])
+
+        # 左右侧被动叠掩
+        if len(np.where(PointDict['layover'] == 1)[0]) != 0:
+            layover_grad_fn = lambda lay_array: [lay_array[i + 1] - lay_array[i] for i in range(len(lay_array) - 1)]
+            PointDict_lay_padding = np.insert(PointDict['layover'], 0, 0)
+            # PointDict_lay_padding=np.append(PointDict_lay_padding,0)
+            layover_grad = np.array(layover_grad_fn(PointDict_lay_padding))
+            if PointDict_lay_padding[-1] == 1:
+                rlayover_start_index = np.where(layover_grad == 1)[0][:-1]
+            else:
+                rlayover_start_index = np.where(layover_grad == 1)[0]
+            rlayover_range_index = np.where(layover_grad == -1)[0]
+            # 右侧叠掩以rlayover_start_index为参考点，rlayover_range_index为像元检索起点，左侧叠掩相反
+            s_h, s_x, s_y, s_lon, s_lat, s_angle = EasyIndex(
+                PointDict, rlayover_start_index, 'height', 'x', 'y', 'longitude', 'latitude', 'incAngle')
+            d_h, d_x, d_y, d_lon, d_lat, d_angle = EasyIndex(
+                PointDict, rlayover_range_index, 'height', 'x', 'y', 'longitude', 'latitude', 'incAngle')
+            # 同时进行左右俩侧像元叠掩检测
+            for i in range(rlayover_start_index.size):
+                start_index = rlayover_start_index[i]
+                range_index = rlayover_range_index[i]
+                index_Rlayover = []
+                index_Llayover = []
+                if range_index + 50 < len(PointDict['layover']):
+                    rangeIndex = range(range_index, range_index + 50)
+                else:
+                    rangeIndex = range(range_index, len(PointDict['layover']))
+
+                if start_index - 50 > 0:
+                    rangeIndex_l = range(start_index - 50, start_index)
+                else:
+                    rangeIndex_l = range(start_index)
+                # 为检测出完整的被动叠掩位置，像元应同时满足其与主动叠掩起始位置和终止位置的几何关系
+                if len(rangeIndex) != 0:
+                    r_Range_h, r_Range_x, r_Range_y, r_Range_lon, r_Range_lat = EasyIndex(
+                        PointDict, rangeIndex, 'height', 'x', 'y', 'longitude', 'latitude')
+                    Rlay_start_iRad = np.arctan(
+                        (r_Range_h - s_h[i]) / np.sqrt(np.square(r_Range_x - s_x[i]) + np.square(r_Range_y - s_y[i])))
+                    Rlay_end_iRad = np.arctan(
+                        (r_Range_h - d_h[i]) / np.sqrt(np.square(r_Range_x - d_x[i]) + np.square(r_Range_y - d_y[i])))
+                    Rlay_start = Rlay_start_iRad * 180 / math.pi
+                    Rlay_end = Rlay_end_iRad * 180 / math.pi
+                    index_Rlayover = np.where(np.logical_and(Rlay_start > s_angle[i], Rlay_end < d_angle[i]))[0]
+
+                if len(rangeIndex_l) != 0:
+                    l_Range_h, l_Range_x, l_Range_y, l_Range_lon, l_Range_lat = EasyIndex(
+                        PointDict, rangeIndex_l, 'height', 'x', 'y', 'longitude', 'latitude')
+                    Llay_start_iRad = np.arctan(
+                        (s_h[i] - l_Range_h) / np.sqrt(np.square(s_x[i] - l_Range_x) + np.square(s_y[i] - l_Range_y)))
+                    Llay_end_iRad = np.arctan(
+                        (d_h[i] - l_Range_h) / np.sqrt(np.square(d_x[i] - l_Range_x) + np.square(d_y[i] - l_Range_y)))
+                    Llay_start = Llay_start_iRad * 180 / math.pi
+                    Llay_end = Llay_end_iRad * 180 / math.pi
+                    index_Llayover = np.where(np.logical_and(Llay_start < s_angle[i], Llay_end > d_angle[i]))[0]
+
+                if len(index_Rlayover) != 0:
+                    tlon_RLay = r_Range_lon[index_Rlayover]
+                    tlat_RLay = r_Range_lat[index_Rlayover]
+                    for j in range(len(tlat_RLay)):
+                        r_lon_sum.append(tlon_RLay[j])
+                        r_lat_sum.append(tlat_RLay[j])
+                if len(index_Llayover) != 0:
+                    tlon_LLay = l_Range_lon[index_Llayover]
+                    tlat_LLay = l_Range_lat[index_Llayover]
+                    for j in range(len(tlat_LLay)):
+                        l_lon_sum.append(tlon_LLay[j])
+                        l_lat_sum.append(tlat_LLay[j])
+
+    rlay_featurecollection = ee.FeatureCollection(
+        [ee.Feature(ee.Geometry.Point(x, y), {'values': 5}) for x, y in zip(r_lon_sum, r_lat_sum)])
+    shadow_featurecollection = ee.FeatureCollection(
+        [ee.Feature(ee.Geometry.Point(x, y), {'values': 3}) for x, y in zip(sh_lon_sum, sh_lat_sum)])
+    llay_featurecollection = ee.FeatureCollection(
+        [ee.Feature(ee.Geometry.Point(x, y), {'values': 4}) for x, y in zip(l_lon_sum, l_lat_sum)])
+    image_rlayover = ee.Image().paint(rlay_featurecollection, 'values')
+    image_llayover = ee.Image().paint(llay_featurecollection, 'values')
+    image_shadow = ee.Image().paint(shadow_featurecollection, 'values')
+    img_rlayover = image_rlayover.clip(AOI_buffer).reproject(crs=proj, scale=scale)
+    img_llayover = image_llayover.clip(AOI_buffer).reproject(crs=proj, scale=scale)
+    img_shadow = image_shadow.clip(AOI_buffer).reproject(crs=proj, scale=scale)
+    passive_img = ee.ImageCollection([img_rlayover, img_llayover, img_shadow]).mosaic()
+    return passive_img
